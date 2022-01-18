1fc49acaa56a617ee406a34bd85aac9ce66275ed
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2022-01-18 21:26:07 +0200

    Data Structures Exercises

---
+++ 2-LinearDataStructures-Exercise/02.DoublyLinkedList/DoublyLinkedList-1.cs
@@ -0,0 +1,129 @@
+﻿//namespace Problem02.DoublyLinkedList
+//{
+//    using System;
+//    using System.Collections;
+//    using System.Collections.Generic;
+
+//    public class DoublyLinkedList<T> : IAbstractLinkedList<T>
+//    {
+//        private Node<T> head;
+//        private Node<T> tail;
+
+//        public int Count { get; private set; }
+
+//        public void AddFirst(T item)
+//        {
+//            Node<T> newHead = new Node<T>(item)
+//            {
+//                Item = item,
+//                Next = this.head
+//            };
+//            if (this.head == null)
+//            {
+//                this.head = newHead;
+//                this.tail = newHead;
+//            }
+//            else
+//            {
+//                this.head.Previous = newHead;
+//                this.head = newHead;
+//            }
+//            Count++;
+//        }
+
+//        public void AddLast(T item)
+//        {
+//            Node<T> newLast = new Node<T>(item)
+//            {
+//                Item = item,
+//                Next = null
+//            };
+
+//            if (this.head == null)
+//            {
+//                this.head = newLast;
+//                this.tail = newLast;
+//            }
+//            else
+//            {
+//                this.tail.Next = newLast;
+//                this.tail = newLast;
+//            }
+
+//            Count++;
+//        }
+
+//        public T GetFirst()
+//        {
+//            if (Count == 0)
+//            {
+//                throw new InvalidOperationException();
+//            }
+
+//            return this.head.Item;
+//        }
+
+//        public T GetLast()
+//        {
+//            if (Count == 0)
+//            {
+//                throw new InvalidOperationException();
+//            }
+
+//            return this.tail.Item;
+//        }
+
+//        public T RemoveFirst()
+//        {
+//            if (Count == 0)
+//            {
+//                throw new InvalidOperationException();
+//            }
+
+//            var newHead = this.head.Next;
+//            var result = this.head.Item;
+//            this.head = newHead;
+//            Count--;
+//            return result;
+//        }
+
+//        public T RemoveLast()
+//        {
+//            if (Count == 0)
+//            {
+//                throw new InvalidOperationException();
+//            }
+
+//            var currentHead = this.head;
+//            var result = this.tail.Item;
+//            while (currentHead.Next != null)
+//            {
+//                if (currentHead.Next.Next == null)
+//                {
+//                    currentHead.Next = null;
+//                    break;
+//                }
+//                currentHead = currentHead.Next;
+//            }
+
+//            Count--;
+//            this.tail = currentHead;
+//            return result;
+//        }
+
+//        public IEnumerator<T> GetEnumerator()
+//        {
+//            var currentHead = this.head;
+//            while (currentHead != null)
+//            {
+//                yield return currentHead.Item;
+//                currentHead = currentHead.Next;
+//            }
+//        }
+
+//        IEnumerator IEnumerable.GetEnumerator()
+//        {
+//            return this.GetEnumerator();
+//        }
+//    }
+//}
---
+++ 2-LinearDataStructures-Exercise/02.DoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,100 @@
+﻿namespace Problem02.DoublyLinkedList
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class DoublyLinkedList<T> : IAbstractLinkedList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public int Count { get; private set; }
+
+        public void AddFirst(T item)
+        {
+            if (this.Count == 0)
+            {
+                this.head = this.tail = new Node<T>(item, null, null);
+                Count++;
+                return;
+            }
+
+            var newNode = new Node<T>(item, this.head, null);
+            this.head.Previous = newNode;
+            this.head = newNode;
+            Count++;
+        }
+
+        public void AddLast(T item)
+        {
+            if (this.Count == 0)
+            {
+                this.head = this.tail = new Node<T>(item, null, null);
+                Count++;
+                return;
+            }
+            var newNode = new Node<T>(item, null, this.tail);
+            this.tail.Next = newNode;
+            this.tail = newNode;
+            Count++;
+        }
+
+        public T GetFirst()
+        {
+            ValidateSize();
+            return this.head.Item;
+        }
+
+        public T GetLast()
+        {
+            ValidateSize();
+            return this.tail.Item;
+        }
+
+        public T RemoveFirst()
+        {
+            ValidateSize();
+            var oldHead = this.head;
+            var newHead = this.head.Next;
+            this.head.Previous = null;
+            this.head = newHead;
+            Count--;
+            return oldHead.Item;
+        }
+
+        public T RemoveLast()
+        {
+            ValidateSize();
+            var oldTail = this.tail;
+            var newTail = this.tail.Previous;
+            this.tail.Next = null;
+            this.tail = newTail;
+            Count--;
+            return oldTail.Item;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var currentHead = this.head;
+            while (currentHead != null)
+            {
+                yield return currentHead.Item;
+                currentHead = currentHead.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private void ValidateSize()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}
